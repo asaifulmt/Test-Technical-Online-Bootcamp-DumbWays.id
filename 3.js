fa67878858a0d53ccf580de48b72ea5ccefbadf8
asaifulmt <asaifulmt@gmail.com>
2021-02-27 20:27:21 +0700

    Jawaban Soal Nomor 3

---
+++ 3.js
@@ -0,0 +1,17 @@
+var arr = [["D"], ["U","M"], 
+          ["B", "W", "A"], 
+          ["Y", "S", "I", "D"], 
+          ["U", "J", "I", "A", "N"]]
+
+var deret = arr.length
+  arr.forEach(function(elemX, indexX) {
+    var teks = " "
+      for (var i = deret - indexX; i>0; i--) {
+        teks = teks + ' '
+      }
+      arr[indexX].forEach(function(elemY, indexY) {
+        teks = teks + arr[indexX][indexY] + " "
+      })
+        console.log(teks)
+    })
+    
